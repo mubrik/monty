2d17db89dbef4372810d972433342c7d049e9742
mubrik <mubarakg4u@gmail.com>
2022-12-15 10:01:29 +0100

    completed free implementation to free all memory blocks

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ m_data_t monty_data;
 
 int main(int argc, char **argv)
 {
-	OP_func *p_func;
+	OP_func *p_func = NULL;
 	FILE *file = NULL;
 	buf line_buf = NULL;
 	ssize_t n_read;
@@ -21,20 +21,20 @@ int main(int argc, char **argv)
 	/* check if argc exactly 2, name and file */
 	if (argc != 2)
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
-
 	/* open file and check */
 	file = fopen(argv[1], "r");
 	if (!file)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
-
 	/* store struct */
-	monty_data.file = file, monty_data.line_buf = line_buf;
-
+	monty_data.file = file;
+	/* main iteration */
 	while ((n_read = getline(&line_buf, &read_l, file)) != -1)
 	{
-		Tokenize(line_buf);
+		/* set to allow free later */
+		monty_data.line_buf = line_buf;
+		Tokenize(line_buf); /* mutates line buf */
+		/* get operator func */
 		p_func = get_func(monty_data.p_data[0]);
-
 		/* update_dtype(monty_data.p_data[0]); */
 		if (p_func)
 			p_func(&monty_data.stk_head, line_number);
